3968e6773065805c5db5e7f053d568b6bd4fef16
abhizala <abhizala@hotmail.com>
2020-10-18 10:32:10 +0530

    Add some more flags
    Make ExtendedUI official and also add blur

--- exui_sargo.mk
+++ exui_sargo.mk
@@ -26,6 +26,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Device specific stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_GAPPS := true
+EXUI_BUILDTYPE := OFFICIAL
+TARGET_USES_BLUR := true
 
 PRODUCT_NAME := exui_sargo
 PRODUCT_DEVICE := sargo
